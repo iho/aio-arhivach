ac88345c43cc10ccd78137f3fe19f92ac0b66c22
iho <ihorgorobets@gmail.com>
2015-03-03 17:13:23 +0200

    init

---
+++ app.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# from __future__ import division, print_function, unicode_literals
+import string
+
+import aiohttp
+import aioredis
+from aiohttp import web
+import random
+
+from urllib.parse import urlparse
+import asyncio
+
+all_chars = string.digits + string.ascii_lowercase + string.ascii_uppercase
+
+
+def gen_hash(length=5):
+    return ''.join(random.choice(all_chars) for x in range(length))
+
+
+@asyncio.coroutine
+def go():
+    redis = yield from aioredis.create_redis(
+        ('localhost', 6379), loop=loop)
+    yield from redis.set('my-key', 'value')
+    val = yield from redis.get('my-key 1')
+    print(val)
+    redis.close()
+
+import ipaddress
+import datetime 
+@asyncio.coroutine
+def index(request):
+    text = "Hello, form"
+#    import ipdb; ipdb.set_trace()
+    return web.Response(body=text.encode('utf-8'))
+
+
+sem = asyncio.Semaphore(5)
+
+
+@asyncio.coroutine
+def index_post(request):
+    "Add block for too much gets from one IP"
+    yield from request.post()
+    url = request.POST['url']
+    if not url.startswith('http://'):
+        url = 'http://' + url
+    ip = request.transport.get_extra_info('peername')[0]
+    ip = ipaddress.ip_address(ip)
+    time = datetime.datetime.now().time()
+    if ip:
+        with (yield from sem):
+            r = yield from aiohttp.request('get', url)
+        raw = yield from r.text()
+        name = gen_hash()
+        yield from redis.set("saved_pages#" + name, raw)
+        return aiohttp.web.HTTPFound('/' + name)
+    return web.Response(body="Error")
+
+
+@asyncio.coroutine
+def page(request):
+    name = request.match_info.get('name', None)
+    if name:
+        body = yield from redis.get("saved_pages#" + name)
+        if body:
+            return web.Response(body=body)
+    return web.HTTPNotFound()
+
+
+@asyncio.coroutine
+def init(loop=None):
+    #    loop = loop or asyncio.get_event_loop()
+    app = aiohttp.web.Application(loop=loop)
+    redis = yield from aioredis.create_redis(
+        ('localhost', 6379), loop=loop)
+
+    app.redis = redis
+
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('POST', '/', index_post)
+    app.router.add_route('GET', '/{name}', page)
+    handler = app.make_handler()
+
+    srv = yield from loop.create_server(handler,
+                                        '127.0.0.1', 8080)
+    print("Server started at http://127.0.0.1:8080")
+    return srv, redis, handler
+
+
+@asyncio.coroutine
+def finish(srv, redis, handler):
+    redis.close()
+    srv.close()
+    yield from handler.finish_connections()
+    yield from srv.wait_closed()
+
+if __name__ == '__main__':
+    #    asyncio.async(asyncio.gather(*run()))
+    loop = asyncio.get_event_loop()
+#    log.info('Crawler start')
+    # need close
+
+    srv, redis, handler = loop.run_until_complete(init(loop))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.run_until_complete(finish(srv, redis, handler))
+        loop.stop()
+       # log.info('Crawler stoped')
